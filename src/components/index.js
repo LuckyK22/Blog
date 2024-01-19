061edd0ccfec99b905b70bf325cfbefc0feffdeb
Lucky Kanathe <100132725+LuckyK22@users.noreply.github.com>
2024-01-19 19:12:37 +0530

    components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,18 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Button from "./Button";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./input";
+import Logo from "./Logo"
+import Container from "./container/Container";
+
 
 export {
     Header,
-    Footer
+    Footer,
+    Button,
+    LogoutBtn,
+    Input,
+    Logo,
+    Container
 }
