0d682a435424ace4bea807fe5fb707780903a5ae
Lucky Kanathe <100132725+LuckyK22@users.noreply.github.com>
2024-01-21 21:44:58 +0530

    Post

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,11 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./input";
 import Logo from "./Logo"
 import Container from "./container/Container";
+import Select from "./Select";
+import RTE from "./RTE";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
 
 
 export {
@@ -14,5 +19,10 @@ export {
     LogoutBtn,
     Input,
     Logo,
-    Container
+    Container,
+    Select,
+    RTE,
+    PostCard,
+    Login,
+    Signup,
 }
